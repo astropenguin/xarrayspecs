5b76cb33f1e1e87d2501442ca6e3fd465dd51f37
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2026-05-02 21:51:41 +0000

    #36 Update tests

--- tests/test_asdataset.py
+++ tests/test_asdataset.py
@@ -7,10 +7,15 @@ import numpy as np
 import xarray as xr
 import xarrayspecs as xs
 from numpy.typing import NDArray
+from pydantic import BaseModel
 
 # type hints
 Lat = Literal["lat"]
 Lon = Literal["lon"]
+
+
+class CustomSet(xr.Dataset):
+    __slots__ = ()
 
 
 @dataclass
@@ -44,6 +49,7 @@ class Weather0(xs.AsDataset):
         xs.attrs(long_name="Longitude", units="deg"),
     ]
     date: Annotated[str, xs.use("attr")]
+    factory: Annotated[type[CustomSet], xs.use("factory")] = CustomSet
 
 
 @dataclass
@@ -65,6 +71,28 @@ class Weather1(xs.AsDataset):
         xs.attrs(long_name="Longitude", units="deg"),
     ]
     date: xs.Attr[str]
+    factory: xs.Factory[CustomSet] = CustomSet
+
+
+class Weather2(BaseModel, xs.AsDataset):
+    temp: Annotated[
+        xs.Data[tuple[Lon, Lat], np.float64],
+        xs.attrs(long_name="Temperature", units="K"),
+    ]
+    humid: Annotated[
+        xs.Data[tuple[Lon, Lat], np.float64],
+        xs.attrs(long_name="Humidity", units="%"),
+    ]
+    lat: Annotated[
+        xs.Coord[Lat, np.float64],
+        xs.attrs(long_name="Latitude", units="deg"),
+    ]
+    lon: Annotated[
+        xs.Coord[Lon, np.float64],
+        xs.attrs(long_name="Longitude", units="deg"),
+    ]
+    date: xs.Attr[str]
+    factory: xs.Factory[CustomSet] = CustomSet
 
 
 def test_asdataset() -> None:
@@ -84,4 +112,13 @@ def test_asdataset() -> None:
         np.array([2, 3]),
         "2026-03-01",
     )
+    np.random.seed(0)
+    ds_2 = Weather2.new(
+        temp=np.random.uniform(273, 293, size=(2, 2)),
+        humid=np.random.uniform(0, 100, size=(2, 2)),
+        lat=np.array([0, 1]),
+        lon=np.array([2, 3]),
+        date="2026-03-01",
+    )
     xr.testing.assert_equal(ds_0, ds_1)  # type: ignore
+    xr.testing.assert_equal(ds_0, ds_2)  # type: ignore
